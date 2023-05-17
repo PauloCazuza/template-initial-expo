caf41229ce18dba67d4cb7e77c8a415f5d72c197
Paulo Roberto Lopes <paulorobertolopes@MacBook-Pro-de-Paulo.local>
2023-05-17 19:07:42 -0300

    project-template

--- App.tsx
+++ App.tsx
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ThemeProvider } from 'styled-components';
+
+import { Roboto_400Regular, Roboto_500Medium, Roboto_700Bold, useFonts } from "@expo-google-fonts/roboto";
+import Routes from '@routes/index';
+import theme from '@theme/index';
+import { ActivityIndicator, StatusBar } from "react-native";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold
+  });
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={theme} >
+      <StatusBar
+        barStyle="dark-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <Routes /> : <ActivityIndicator />}
+    </ThemeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/theme/index.ts
@@ -0,0 +1,38 @@
+export default {
+    COLORS: {
+        WHITE: '#FFFFFF',
+        BLACK: '#000000',
+
+        GREEN_700: '#00875F',
+        GREEN_500: '#00B37E',
+
+        TURQUOISE: '#019688',
+        TURQUOISE_LIGHT: '#C3E6E5',
+        TURQUOISE_EXTRA_LIGHT: '#7FCAC3',
+
+        RED: '#F75A68',
+        RED_DARK: '#AA2834',
+
+        GRAY_700: '#121214',
+        GRAY_600: '#616161',
+        GRAY_500: '#29292E',
+        GRAY_400: '#323238',
+        GRAY_300: '#ADADAD',
+        GRAY_200: '#C4C4CC',
+        GRAY_100: '#F1F1F1',
+        GRAY_050: '#F5F5F5',
+    },
+    FONT_FAMILY: {
+        REGULAR: 'Roboto_400Regular',
+        MEDIUM: 'Roboto_500Medium',
+        BOLD: 'Roboto_700Bold'
+    },
+    FONT_SIZE: {
+        SSM: 12,
+        SM: 14,
+        MD: 16,
+        LG: 18,
+        XL: 24,
+        XXL: 32
+    }
+};
